8d173fc23f78c7ab3024cc7955fee9eed13a8edd
johao-dev <johaobravoespinoza1501@gmail.com>
2026-09-03 14:07:29 -0500

    feat: Remove RolUpsertDto class to streamline role management structure

--- Cobranzas_Vittoria/Dtos/Seguridad/RolUpsertDto.cs
+++
@@ -1,9 +0,0 @@
-namespace Cobranzas_Vittoria.Dtos.Seguridad
-{
-    public class RolUpsertDto
-    {
-        public int? IdRol { get; set; }
-        public string Nombre { get; set; } = string.Empty;
-        public bool Activo { get; set; } = true;
-    }
-}
