7933661502d2dc093e9c683fc1d0c38cb5d462ab
johao-dev <johaobravoespinoza1501@gmail.com>
2026-09-08 12:06:36 -0500

    feat: Remove default value comment from IUsuarioActualService to prepare for future authentication integration

--- Cobranzas_Vittoria/Seguridad/Application/Common/IUsuarioActualService.cs
+++ Cobranzas_Vittoria/Seguridad/Application/Common/IUsuarioActualService.cs
@@ -2,8 +2,6 @@ namespace Cobranzas_Vittoria.Seguridad.Application.Common;
 
 /// <summary>
 /// Servicio de aplicacion que resuelve el usuario que ejecuta la operacion.
-/// Por ahora devuelve un valor default ("sistema") hasta que se conecte
-/// la autenticacion via HttpContext.User.
 /// </summary>
 public interface IUsuarioActualService
 {
